6c2ceb7818dbdaeb6148a7c96abd347c7240c028
Rosa Medina <rmmgomez@gmail.com>
2025-10-01 12:42:59 +0200

    cambios

--- JS/Part3 - DOM/7. innerHTML.js
+++ JS/Part3 - DOM/7. innerHTML.js
@@ -3,7 +3,7 @@ const form = document.getElementById("form");
 form.addEventListener('submit', e => {
     e.preventDefault();
     const texto = form.texto.value;
-    document.getElementById("p1").innerHTML = texto;
+    document.getElementById("p1").textContent = texto;
 });
 
 /* 
